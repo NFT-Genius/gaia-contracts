e79f820b36f7712ac5f0273543930a74e7f8524d
JawwadNFTGenius <91911310+JawwadNFTGenius@users.noreply.github.com>
2021-10-26 01:01:59 +0500

    Add files via upload

--- unit-tests/src/config.js
+++ unit-tests/src/config.js
@@ -1,7 +1,6 @@
 import {config} from "@onflow/fcl"
+
 config()
-.put("accessNode.api", process.env.ACCESS_NODE)
-.put("challenge.handshake", process.env.WALLET_DISCOVERY)
+.put("accessNode.api", process.env.ACCESS_NODE) 
+.put("challenge.handshake", process.env.WALLET_DISCOVERY) 
 .put("0xProfile", process.env.APP_CONTRACT_PROFILE)
-//.put("discovery.wallet.method", process.env.WALLET_DISCOVERY_METHOD)
-//.put("discovery.wallet", process.env.WALLET_DISCOVERY)
