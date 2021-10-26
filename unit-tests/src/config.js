b2464ad42b553e9610d3695e60894e2fc3a0f692
JawwadNFTGenius <91911310+JawwadNFTGenius@users.noreply.github.com>
2021-10-26 23:51:58 +0500

    Delete unit-tests/src directory

--- unit-tests/src/config.js
+++
@@ -1,6 +0,0 @@
-import {config} from "@onflow/fcl"
-
-config()
-.put("accessNode.api", process.env.ACCESS_NODE) 
-.put("challenge.handshake", process.env.WALLET_DISCOVERY) 
-.put("0xProfile", process.env.APP_CONTRACT_PROFILE)
