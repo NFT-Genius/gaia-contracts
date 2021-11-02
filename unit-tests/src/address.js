98f4b6006889bc890d430cccfd607b4a73cb36e2
JawwadNFTGenius <91911310+JawwadNFTGenius@users.noreply.github.com>
2021-11-02 23:57:00 +0500

    Delete unit-tests/src directory

--- unit-tests/src/address.js
+++
@@ -1,9 +0,0 @@
-export const sansPrefix = (address) => {
-  if (address == null) return null;
-  return address.replace(/^0x/, "");
-};
-
-export const withPrefix = (address) => {
-  if (address == null) return null;
-  return "0x" + sansPrefix(address);
-};
