c2c1696d1489fd2f122a5b2474b548f33d873bb4
JawwadNFTGenius <91911310+JawwadNFTGenius@users.noreply.github.com>
2021-11-02 23:36:01 +0500

    Update 02_deploy_contract.test.js

--- unit-tests/tests/02_deploy_contract.test.js
+++ unit-tests/tests/02_deploy_contract.test.js
@@ -1,11 +1,9 @@
 import path from "path";
-import {
-  emulator,
-  init,
-  deployContractByName,
-  getContractAddress,
-  getServiceAddress,
-} from "../src";
+import emulator from "../src/emulator";
+import init from "../src/init";
+import deployContractByName from "../src/deploy-code";
+import getContractAddress from "../src/contract";
+import getServiceAddress from "../src/manager";
 
 jest.setTimeout(10000);
 
@@ -31,27 +29,27 @@ describe("deploying all contracts", () => {
     expect(address).toBe(serviceAccount);
   });
 
-  test("Gaia contract deploy", async () => {
-    const name = "Gaia";
-    await deployContractByName(name);
-    const address = await getContractAddress(name);
-    const serviceAccount = await getServiceAddress();
-    expect(address).toBe(serviceAccount);
-  });
+  // test("Gaia contract deploy", async () => {
+  //   const name = "Gaia";
+  //   await deployContractByName(name);
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
 
-  test("Profile contract deploy", async () => {
-    const name = "Profile";
-    await deployContractByName(name);
-    const address = await getContractAddress(name);
-    const serviceAccount = await getServiceAddress();
-    expect(address).toBe(serviceAccount);
-  });
+  // test("Profile contract deploy", async () => {
+  //   const name = "Profile";
+  //   await deployContractByName(name);
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
 
-  test("NFTStorefront contract deploy", async () => {
-    const name = "NFTStorefront";
-    await deployContractByName(name);
-    const address = await getContractAddress(name);
-    const serviceAccount = await getServiceAddress();
-    expect(address).toBe(serviceAccount);
-  });
+  // test("NFTStorefront contract deploy", async () => {
+  //   const name = "NFTStorefront";
+  //   await deployContractByName(name);
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
 });
