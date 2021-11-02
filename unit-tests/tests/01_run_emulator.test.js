8164330a6c21ee6373e5963cdb8adf9599a520f9
JawwadNFTGenius <91911310+JawwadNFTGenius@users.noreply.github.com>
2021-11-03 00:46:03 +0500

    Add files via upload

---
+++ unit-tests/tests/01_run_emulator.test.js
@@ -0,0 +1,31 @@
+import path from "path";
+import { emulator, init } from "flow-js-testing";
+
+jest.setTimeout(10000);
+
+describe("unit-tests", ()=>{
+  beforeEach(async () => {
+    const basePath = path.resolve(__dirname, "../"); 
+		const port = 8080; 
+		const logging = false;    
+    await init(basePath, { port });
+    return emulator.start(port, logging);
+  });
+  
+  afterEach(async () => {
+    return emulator.stop();
+  });
+  
+  test("shall format single info line properly", async () => {
+    const msg = "Hello, world!";
+    const output = emulator.parseDataBuffer(msg);
+    console.log({ output });
+  });
+
+  test("shall format logged message", ()=>{
+    const msg =`time="2021-11-03T18:06:56+05:00" level=info msg=" Starting gRPC server on port 3569" port=3569`
+    const output = emulator.parseDataBuffer(msg);
+    console.log({ output })
+    expect(output.level).toBe("info")
+  })
+})
---
+++ unit-tests/tests/02_deploy_contract.test.js
@@ -0,0 +1,57 @@
+import path from "path";
+import { 
+  emulator, 
+  init, 
+  deployContractByName, 
+  getContractAddress, 
+  getServiceAddress 
+} from "flow-js-testing";
+
+jest.setTimeout(10000);
+
+describe("deploying all contracts", () => {
+  // Instantiate emulator and path to Contracts
+  beforeEach(async () => {
+    const basePath = path.resolve(__dirname, "../");
+    const port = 8080;
+    await init(basePath, { port });
+    return emulator.start(port);
+  });
+
+  // Stop emulator
+  afterEach(async () => {
+    return emulator.stop();
+  });
+
+  test("NonFungibleToken contract deploy", async () => {
+    const name = "NonFungibleToken";
+    await deployContractByName(name);
+    const address = await getContractAddress(name);
+    const serviceAccount = await getServiceAddress();
+    expect(address).toBe(serviceAccount);
+  });
+
+  // test("Gaia contract deploy", async () => {
+  //   const name = "Gaia";
+  //   await deployContractByName({ name });
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
+
+  // test("Profile contract deploy", async () => {
+  //   const name = "Profile";
+  //   await deployContractByName({ name });
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
+
+  // test("NFTStorefront contract deploy", async () => {
+  //   const name = "NFTStorefront";
+  //   await deployContractByName(name);
+  //   const address = await getContractAddress(name);
+  //   const serviceAccount = await getServiceAddress();
+  //   expect(address).toBe(serviceAccount);
+  // });
+});
