89822e7fb6e6b0f1201709878479aeb4545f46e1
Carlos Tosta <jctosta86@gmail.com>
2021-11-23 13:57:12 -0300

    Flow json configuration update and linter fixes

--- tests/02_deploy_contracts.test.js
+++ tests/02_deploy_contracts.test.js
@@ -1,18 +1,19 @@
-import path from "path";
-import { 
-  emulator, 
-  init, 
-  deployContractByName, 
-  getContractAddress, 
-  getServiceAddress 
-} from "flow-js-testing";
+/* eslint-disable no-undef */
+import path from 'path';
+import {
+  emulator,
+  init,
+  deployContractByName,
+  getContractAddress,
+  getServiceAddress
+} from 'flow-js-testing';
 
 jest.setTimeout(10000);
 
-describe("deploying all contracts", () => {
+describe('deploying all contracts', () => {
   // Instantiate emulator and path to Contracts
   beforeEach(async () => {
-    const basePath = path.resolve(__dirname, "../");
+    const basePath = path.resolve(__dirname, '../');
     const port = 8080;
     await init(basePath, { port });
     return emulator.start(port);
@@ -23,10 +24,11 @@ describe("deploying all contracts", () => {
     return emulator.stop();
   });
 
-  test("Profile contract deploy", async () => {
-    const name = "Profile";
+  test('Profile contract deploy', async () => {
+    const name = 'Profile';
     await deployContractByName(name);
     const address = await getContractAddress(name);
+    console.log({ address });
     const serviceAccount = await getServiceAddress();
     expect(address).toBe(serviceAccount);
   });
